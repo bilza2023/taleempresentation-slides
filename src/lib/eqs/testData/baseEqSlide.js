0093db1f4c77cd69d7f69b13642a64a80aaac898
bilza2023 <bilza2023@gmail.com>
2024-11-25 22:58:08 +0500

    the demo slide for eq editor is perfect - means show all the capabilities.

--- src/lib/eqs/testData/baseEqSlide.js
+++ src/lib/eqs/testData/baseEqSlide.js
@@ -1,160 +1,122 @@
-export const Slide ={
-    "items": [
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 0,
-          "startTime": 0,
-          "endTime": 5,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "Write the following quadratic equations in the standard form and point out pure quadratic equations.",
-          "type": "hdg",
-          "sp": [],
-          "selected": true,
-        },
-       
-      },
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 1,
-          "startTime": 5,
-          "endTime": 10,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "(x+7)(x-3)=-7",
-          "type": "code",
-          "sp": [
-            {
-              "code": "Solution",
-              "type": "text"
-            },
-            {
-              "code": "https://taleem-media.blr1.cdn.digitaloceanspaces.com/bucket/baloons.png",
-              "type": "img"
-            }
-          ],
-          "fs": [],
-          "spVisibility": false,
-          "fsVisibility": false
-        },
-        "arr": [],
-        "_id": {
-          "$oid": "65c30b3ecd2ccbad64e22326"
-        }
-      },
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 2,
-          "startTime": 10,
-          "endTime": 20,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "(x+7)(x-3)=-7",
-          "type": "code",
-          "sp": [
-            {
-              "code": "Multiply the equation",
-              "type": "text"
-            }
-          ],
-          "fs": [],
-          "spVisibility": false,
-          "fsVisibility": false
-        },
-        "arr": [],
-        "_id": {
-          "$oid": "65c30b3ecd2ccbad64e22327"
-        }
-      },
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 3,
-          "startTime": 20,
-          "endTime": 30,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "x^2+4x-21=-7",
-          "type": "code",
-          "sp": [],
-          "fs": [],
-          "fsVisibility": false,
-          "spVisibility": false
-        },
-        "arr": [],
-        "_id": {
-          "$oid": "65c30b3ecd2ccbad64e22328"
-        }
-      },
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 4,
-          "startTime": 30,
-          "endTime": 40,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "x^2+4x-21+7=7",
-          "type": "code",
-          "sp": [],
-          "fs": [],
-          "fsVisibility": false,
-          "spVisibility": false
-        },
-        "arr": [],
-        "_id": {
-          "$oid": "65c30b3ecd2ccbad64e22329"
-        }
-      },
-      {
-        "name": "",
-        "content": "",
-        "showAt": 0,
-        "itemExtra": {
-          "step": 5,
-          "startTime": 40,
-          "endTime": 50,
-          "fsStartTime": 0,
-          "fsEndTime": 0,
-          "code": "x^2+4x-14=0",
-          "type": "code",
-          "sp": [
-            {
-              "code": "It is standard form of  quadratic equation(Standard Form)",
-              "type": "text"
-            },
-            {
-              "code": "ax^2+bx+c=0",
-              "type": "code"
-            },
-            {
-              "code": "Answer",
-              "type": "text"
-            }
-          ],
-          "fs": [],
-          "spVisibility": true,
-          "fsVisibility": false
-        },
-        "arr": [],
-        "_id":  "65c30b3ecd2ccbad64e2232a"
-        
+export const Slide = {
+  "items": [
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 0,
+        "endTime": 5,
+        "code": "Write the following quadratic equations in the standard form and point out pure quadratic equations.",
+        "type": "hdg",
+        "sp": []
       }
-    ],
-    "slideExtra": [],
-   
+    },
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 5,
+        "endTime": 10,
+        "code": "(x+7)(x-3)=-7",
+        "type": "code",
+        "sp": [
+          {
+            "code": "https://taleem-media.blr1.cdn.digitaloceanspaces.com/bucket/baloons.png",
+            "type": "img"
+          }
+        ]
+      },
+      "_id": { "$oid": "65c30b3ecd2ccbad64e22326" }
+    },
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 10,
+        "endTime": 15,
+        "code": "(x+7)(x-3)=-7",
+        "type": "code",
+        "sp": [
+          {
+            "code": "This is Heading",
+            "type": "heading"
+          },
+          {
+            "code": "This is text",
+            "type": "text"
+          },
+          {
+            "code": "\\sqrt{555}",
+            "type": "code"
+          }
+        ]
+      },
+      "_id": { "$oid": "65c30b3ecd2ccbad64e22327" }
+    },
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 15,
+        "endTime": 20,
+        "code": "x^2+4x-21=-7",
+        "type": "code",
+        "sp": [
+          {
+            "code": "[[\"\\\\sqrt{44}\",\"\\\\sqrt{44}\"],[\"\\\\sqrt{44}\",\"\\\\sqrt{44}\"]]",
+            "type": "tableCode"
+          },
+          {
+            "code": "[[\"jj\",\"kk\"],[\"99\",\"ii\"]]",
+            "type": "table"
+          }
+        ]
+      },
+      "_id": { "$oid": "65c30b3ecd2ccbad64e22328" }
+    },
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 20,
+        "endTime": 25,
+        "code": "x^2+4x-21+7=7",
+        "type": "code",
+        "sp": []
+      },
+      "_id": { "$oid": "65c30b3ecd2ccbad64e22329" }
+    },
+    {
+      "name": "",
+      "content": "",
+      "showAt": 0,
+      "itemExtra": {
+        "startTime": 25,
+        "endTime": 30,
+        "code": "x^2+4x-14=0",
+        "type": "code",
+        "sp": [
+          {
+            "code": "It is standard form of  quadratic equation(Standard Form)",
+            "type": "text"
+          },
+          {
+            "code": "ax^2+bx+c=0",
+            "type": "code"
+          },
+          {
+            "code": "Answer",
+            "type": "text"
+          }
+        ]
+      },
+      "_id": "65c30b3ecd2ccbad64e2232a"
+    }
+  ],
+  "slideExtra": []
 };
-
